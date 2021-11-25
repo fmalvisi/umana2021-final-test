a12a43645970116828c175e20f2993bf2027557c
Nicolò Matassoli <nico.matassoli@gmail.com>
2021-11-25 21:13:36 +0100

    aggiunte call api

--- src/app/modules/categories/components/categories-detail/categories-detail.component.ts
+++ src/app/modules/categories/components/categories-detail/categories-detail.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { CategoryService, ItemsService } from 'src/app/core/api/generated';
 
 @Component({
   selector: 'app-categories-detail',
   templateUrl: './categories-detail.component.html',
-  styleUrls: ['./categories-detail.component.scss']
+  styleUrls: ['./categories-detail.component.scss'],
 })
 export class CategoriesDetailComponent implements OnInit {
-
-  constructor() { }
-
+  name = '';
+  description = '';
+  items = '';
+  constructor(
+    private categoryService: CategoryService,
+    private itemService: ItemsService,
+  ) {}
   ngOnInit(): void {
-  }
+    this.categoryService.getCategories().subscribe(cats => {
+      for (const item of cats) {
+        this.name = item.name;
+        this.description = item.description;
+      }
+    });
 
+    this.itemService.getItems().subscribe(items => {
+      for (const item of items) {
+        console.log(item.name);
+        this.items += item.name;
+      }
+    });
+  }
 }
