e18099a6231212665c578f33df1699a3a889c5c1
Francesco <francescopapps@gmail.com>
2021-11-25 18:09:19 +0100

    aggiunto routing per i componenti creati

--- src/app/modules/items/items-routing.module.ts
+++ src/app/modules/items/items-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddItemComponent } from './add-item/add-item.component';
+import { DetailsItemComponent } from './details-item/details-item.component';
+import { ListItemsComponent } from './list-items/list-items.component';
+import { ModifyItemComponent } from './modify-item/modify-item.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ListItemsComponent,
+    children: [
+      {
+        path: 'aggiungi-oggetto',
+        component: AddItemComponent
+      },
+      {
+        path: 'dettagli-oggetto',
+        component: DetailsItemComponent
+      },
+      {
+        path: 'modifica-oggetto',
+        component: ModifyItemComponent
+      }
+    ]}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
