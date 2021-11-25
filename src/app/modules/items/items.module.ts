6d82a077ba090c2184697208477e07231207c96b
carloFanc <fanciullicarlo@gmail.com>
2021-11-25 17:21:02 +0100

    creazione dei componenti

--- src/app/modules/items/items.module.ts
+++ src/app/modules/items/items.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ItemsRoutingModule } from './items-routing.module';
+import { ListItemsComponent } from './list-items/list-items.component'; 
+import { AddItemComponent } from './add-item/add-item.component';
+import { DetailsItemComponent } from './details-item/details-item.component';
+import { ModifyItemComponent } from './modify-item/modify-item.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ListItemsComponent, 
+    AddItemComponent,
+    DetailsItemComponent,
+    ModifyItemComponent
+  ],
   imports: [
     CommonModule,
     ItemsRoutingModule
---
+++ src/app/modules/items/modify-item/modify-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modify-item',
+  templateUrl: './modify-item.component.html',
+  styleUrls: ['./modify-item.component.scss']
+})
+export class ModifyItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
