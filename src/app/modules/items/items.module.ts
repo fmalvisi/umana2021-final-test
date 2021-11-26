13ce44e8f9dc9e9ce48b0c76f0939bb8771ebb61
serena <serena.russo8@studio.unibo.it>
2021-11-26 11:22:17 +0100

    importato form module in items.modle.ts

--- src/app/modules/items/items.module.ts
+++ src/app/modules/items/items.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 
 import { ItemsRoutingModule } from './items-routing.module';
 import { ListItemsComponent } from './list-items/list-items.component'; 
@@ -21,7 +23,8 @@ import { CurrencyEurPipe } from './../../shared/pipes/currency.pipe';
   ],
   imports: [
     CommonModule,
-    ItemsRoutingModule
+    ItemsRoutingModule,
+    FormsModule
   ]
 })
 export class ItemsModule { }
--- src/app/modules/items/modify-item/modify-item.component.ts
+++ src/app/modules/items/modify-item/modify-item.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CategoryService, Item, ItemsService, UsersService } from '../../../core/api/generated';
+
 
 @Component({
   selector: 'app-modify-item',
@@ -6,10 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./modify-item.component.scss']
 })
 export class ModifyItemComponent implements OnInit {
+  name_objects="nome_prova_pipe";
+  description="descrizione_prova_pipe";
+  price="14.3";
+  url_photo="url_prova_pipe";
+
+  send(){
+    var name_prov = document.getElementById("name_input") as HTMLInputElement
+    this.name_objects= name_prov.value;
+    console.log(this.name_objects)
+  }
+  
+
+
 
   constructor() { }
 
   ngOnInit(): void {
+    console.log(this.name_objects)
+
   }
+
+
 
 }
