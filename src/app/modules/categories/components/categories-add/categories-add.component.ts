7cff79eafe5825505d0fe3bea40021f002b88c33
sumrge <sumrge41@protonmail.com>
2021-11-26 12:04:35 +0100

    add versione base

--- src/app/modules/categories/components/categories-add/categories-add.component.ts
+++ src/app/modules/categories/components/categories-add/categories-add.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { CategoryService, ItemsService } from 'src/app/core/api/generated';
+
 
 @Component({
   selector: 'app-categories-add',
