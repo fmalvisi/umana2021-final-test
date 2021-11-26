0bb829f2ad716467272c5bf6b8d2947ccbc6e52c
Francesco <francescopapps@gmail.com>
2021-11-26 14:20:06 +0100

    aggiornata la listItems

--- src/app/modules/items/list-items/list-items.component.ts
+++ src/app/modules/items/list-items/list-items.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core'; 
 import { Observable, of } from 'rxjs';
 import { SuperItemService } from 'src/app/core/services/superItemService';
-import { Item, ItemsService } from '../../../core/api/generated';
+import { Item } from '../../../core/api/generated';
  
 
 @Component({
@@ -20,12 +20,11 @@ export class ListItemsComponent implements OnInit {
   
   constructor(
     private superService : SuperItemService,
-    private itemsService : ItemsService,
     ) {}
 
   ngOnInit(): void {     
      this.items = this.superService.getItemList();
-     console.log(this.items);
+     console.log('itemsaaaaaaaaaa',this.items);
      this.superService.getProva(1).subscribe(res => {
        this.item = res;
        console.log("dentro",this.item)
