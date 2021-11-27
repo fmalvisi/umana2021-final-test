2d31531ed4af9b288ac8ce03949631e8a1a8db35
StefanoDonati <crown.donas@gmail.com>
2021-11-27 14:30:51 +0100

    aggiunta funzionalità filtro categories

--- src/app/modules/categories/components/categories-table/categories-table.component.ts
+++ src/app/modules/categories/components/categories-table/categories-table.component.ts
@@ -63,10 +63,28 @@ export class CategoriesTableComponent implements OnInit {
       console.log('assigned categories: ', this.categoryArr);
     });
 
+  }
 
-   
+  filterCategory(c: string):void{
+    console.log('selezionata categoria: ', c);
+    let toHide: string[] = [];
+    let toShow: string = c.toLowerCase();
 
-    console.log('array categorie ordinato: ' ,this.categoryArr)
+    for(let category of this.categoryArr){
+      if(category.name !== c){
+        toHide.push(category.name.toLowerCase());
+      }
+    }
+    console.log('categorie da nascondere: ', toHide);
+    for(let h of toHide){
+      if(toHide.length < this.categoryArr.length){
+        document.getElementById(h)?.classList.add('hidden');
+      }else {
+        document.getElementById(h)?.classList.remove('hidden');
+      } 
+    }
+    document.getElementById(toShow)?.classList.remove('hidden');
+
   }
 
   updateCurrentCategory(categoryID:number | null){
