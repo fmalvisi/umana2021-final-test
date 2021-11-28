31273c56c0098a7f37f31af041ee6b2d22102afe
Francesco <francescopapps@gmail.com>
2021-11-28 19:01:26 +0100

    ultimati i filtri per gli items

--- src/app/modules/items/list-items/list-items.component.ts
+++ src/app/modules/items/list-items/list-items.component.ts
@@ -13,35 +13,31 @@ export class ListItemsComponent implements OnInit {
   
   message: string|null = null;
   items: Item[] = [];
+  itemsFiltrati: Item[] = [];
   categories: Category[] = [];
   users: User[] = [];
   item? : Item;
-  // itemProva$?: Observable<Item>;
-  // itemProva2$ = of(true);
   mostra = true;
   mostraFiltri = false;
+  oggettiFiltrati = false;
   saveUser: User[] = [];
   searchUser: User[] = [];
   saveItems: Item[] = [];
   searchItems: Item[] = [];
-  filtriInUso: any[] = [{tipo: '', id: 0, nome:''}];
+  filtriInUso = [{tipo: '', id: 0, nome:''}];
   
   constructor(
     private superService : SuperItemService,
     private router : Router
   ) {}
 
-  ngOnInit(): void {     
-    //  this.superService.getProva(1).subscribe(res => {
-    //    this.item = res;
-    //  })
-    //  this.itemProva$ = this.superService.getProva(1);
+  ngOnInit(): void {      
     this.getItems();    
+    this.filtriInUso.splice(0,1);
   }
 
   returnHome() {
-    this.router.navigate(['/']);
-    
+    this.router.navigate(['/']);    
   }
 
   modify(index: number){
@@ -134,26 +130,71 @@ export class ListItemsComponent implements OnInit {
   }
 
   applicaFiltri(){
-
+    console.log("Numero di filtri utilizzati: ",this.filtriInUso.length);
+    if(this.filtriInUso.length !== 0){
+      this.getFilteredItems();
+      this.oggettiFiltrati = true;
+    } else {
+      this.oggettiFiltrati = false;
+      this.getItems();
+    }
+    
   }
 
   rimuoviFiltri(){
     this.filtriInUso = [];
+    window.location.reload();
   }
 
   chosenFilter(type: string, idF: number, name: string, event?: any){
     let tmpArray = {tipo: type, id: idF, nome: name};
-    console.log(event === undefined);
     if(event === undefined || event.target.checked === true){
       this.filtriInUso.push(tmpArray);
-    // } else if(event !== undefined || event.target.cheched === false){
-    //   for(let i=0; i<this.filtriInUso.length; i++){
-    //     if(this.filtriInUso[i] === tmpArray){
-    //       this.filtriInUso.slice(i, 1);
-    //     }
-    //   }
+    } else if(event !== undefined || event.target.cheched === false){
+      for(let i=0; i<this.filtriInUso.length; i++){
+        if(this.filtriInUso[i].tipo === type && this.filtriInUso[i].id === idF){
+          this.filtriInUso.splice(i, 1);
+        }
+      }
     }
     
     console.log("filtri in uso", this.filtriInUso)
   }
+
+  removeOneFilter(tipo: string, id:number, nome:string){
+    if(tipo !== 'categoria'){
+      for(let i=0; i<this.filtriInUso.length; i++){
+        let filtro = this.filtriInUso[i];
+        if(filtro.tipo === tipo && filtro.id === id && filtro.nome === nome){
+          this.filtriInUso.splice(i, 1);
+        }
+      }
+    }
+    console.log("se vuoi rimuovere il filtro di una categoria devi usare lo switch");
+  }
+
+
+  getFilteredItems(){
+    this.itemsFiltrati = [];
+    this.superService.getItemList().then((res: Item[]) => {
+      for(let item of res){
+        for(let filtro of this.filtriInUso){
+          if(filtro.tipo === 'categoria' && filtro.id === item.category){
+            this.itemsFiltrati.push(item);
+          } else {
+            if(filtro.tipo === 'utente' && filtro.id === item.owner){
+              this.itemsFiltrati.push(item);
+            } else if (filtro.id === item.id && filtro.nome === item.name) {
+              this.itemsFiltrati.push(item);
+            }
+          }
+        }
+      }
+    }).catch((error) => {
+      window.alert("errore di chiamata API" + error);
+      this.returnHome;
+    })
+  }
+
+
 }
