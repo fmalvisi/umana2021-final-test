0edd151750e9cf1fd741f00a1fb68a68b772bfd0
serena <serena.russo8@studio.unibo.it>
2021-11-28 22:35:48 +0100

    proprietario modify

--- src/app/modules/items/modify-item/modify-item.component.ts
+++ src/app/modules/items/modify-item/modify-item.component.ts
@@ -17,13 +17,18 @@ export class ModifyItemComponent implements OnInit {
   price_euro=0;
   price_centesimi=0;
   category_objects=0
-  category_objects_selected="";
+  category_objects_selected="";//
+  user_array:string[]=[]; //salvataggio di tutte le categorie
+
   url_photo="";
   owner=0;
   owner_objects_selected="";
+  
   //owner_objects_selected="";
   //owner_selected_id=0;
   prova="";
+
+  prov="";
 
 
   url_img_input=""
@@ -69,6 +74,7 @@ export class ModifyItemComponent implements OnInit {
       this.getCategories();
       this.getUser();
       
+      
     }).catch((error) => {
       window.alert("errore di chiamata API" + error);
     })
@@ -80,11 +86,12 @@ export class ModifyItemComponent implements OnInit {
   getCategories(){ 
     this.superService.getCategoryList().then((res: Category[]) => {
       this.categories=res;
+     
+  
 
       if(this.category_objects!=null){
         this.getCategoryId(this.category_objects)
       }
-
 
     }).catch((error) => {
       window.alert("errore di chiamata API" + error);
@@ -99,42 +106,80 @@ export class ModifyItemComponent implements OnInit {
     this.superService.getCategoryById(id).then((res:Category)=>{
       this.category=res;
 
+       this.prov = this.category.name;
+      
       //imposto valore category_objects_selected
       let controller_name=this.category.name;
+
+     
       if(controller_name!==null){
-      this.category_objects_selected=this.category.name;
+        this.category_objects_selected=this.category.name;
       }
 
     }).catch((error)=>{
       window.alert("errore di chiamata API" + error);
     })
   }
+
+  
 
   //users
   //richiamato in getItemId()
   getUser(){
     this.superService.getUserList().then((res:User[])=>{
       this.users=res;
-
-      if(this.owner!=null){
+     /* if(this.owner!=null){
         this.getUserId(this.owner)
+      }*/
+
+      /*riempimento user_array*/
+      for(let i =1; i<this.users.length+1; i++){
+      this.getUserId(i)
       }
     });
   }
 
   //richiamato in getUser()
   getUserId(id:number){
-    this.superService.getUserById(id).then((res:User)=>{
+  this.superService.getUserById(id).then((res:User)=>{
       this.user=res;
+
+      this.user_array.push(this.user.name + " " + this.user.surname)
+      //console.log("CCC" + this.user_array)
 
       //imposto valore owner_objects_selected
       if(this.owner!==null){
         this.owner_objects_selected=this.user.name + " " + this.user.surname;
+        this.sortArrayUser();
+
       }
+
 
     }).catch((error)=>{
       window.alert("errore di chiamata API" + error);
     })
+  }
+
+
+  //ordinamento corretto array
+  sortArrayUser(){
+    let i_1;
+
+    for(let i =0; i<this.user_array.length;i++){
+      i_1 =this.user_array[0];
+
+      console.log(this.user_array[i])
+      if(this.user_array[i]===this.owner_objects_selected){
+        /*let i_x = this.user_array[i];
+        this.user_array[0]=i_x;
+        this.user_array[i]=i_1;*/
+        console.log("Trovato match " + i_1 + " " + this.user_array[i])
+
+        //this.user_array.splice(i);
+      }
+      
+    }
+
   }
 
     showCarouselItems(){
@@ -145,16 +190,18 @@ export class ModifyItemComponent implements OnInit {
       }
     }
 
-    constructor( private superService : SuperItemService, private route: ActivatedRoute, 
-      ) { }
-
-
     onFormSubmit(userForm: NgForm) {
       console.log(userForm.value);
       console.log('NOME:' + userForm.controls["name_objects"].value);
      
       //console.log('Password:' + userForm.controls['pass'].value);
   }
+
+    constructor( private superService : SuperItemService, private route: ActivatedRoute, 
+      ) { }
+
+
+    
 
   ngOnInit(): void {
 
@@ -169,6 +216,7 @@ export class ModifyItemComponent implements OnInit {
     this.owner_objects_selected="Nessun utente assegnato"
 
     this.getItemsId(this.id);
+    
   }
 
 }
