eb0663733300dd40e51e40ba03738e0bdd0afc78
carloFanc <fanciullicarlo@gmail.com>
2021-11-29 23:45:54 +0100

    fix grafico + test listitemPage

--- e2e-tests/cypress/integration/listitemPage.test.spec.js
+++ e2e-tests/cypress/integration/listitemPage.test.spec.js
@@ -6,11 +6,11 @@ describe('listitemPage tests', () => {
     cy.visit("/#/items");  
   });
 
-  it('clicking on the navbar the item section', () => {
+  xit('clicking on the navbar the item section', () => {
     cy.url().should('contain', 'items'); 
   });
 
-  it('should delete one item and return in item path', () => {
+  xit('should delete one item and return in itemlist path', () => {
     
     cy.get('#carouselExampleIndicators div.card').then(cards => {
       const initialElements = cards.length;
@@ -33,5 +33,89 @@ describe('listitemPage tests', () => {
   cy.url().should('eq', 'http://localhost:4200/#/items');
   });
  
+  it('testing category Casa filter', () => {
+    
+    cy.get('#carouselExampleIndicators div.card').then(cards => {
+      const initialElements = cards.length;
+      const cardsExpected = 3;
+    cy.get('button#btnFiltriF')
+    .click(); 
+
+    cy.get('button#categoryFilter')
+    .click();
+
+    cy.get('#flexSwitchCheckDefault')
+    .first()
+    .check();
+
+    cy.get('button#btnFiltriA')
+    .click();
+
+    cy.wait(1000);
+
+    cy.get('div.card2')
+    .then(cards => { 
+      expect(cards.length).equal(cardsExpected);
+    });
+  }); 
+  });
+
+  it('testing user Mario Rossi filter', () => {
+    
+    cy.get('#carouselExampleIndicators div.card').then(cards => {
+      const initialElements = cards.length;
+      const cardsExpected = 1;
+    cy.get('button#btnFiltriF')
+    .click(); 
+
+    cy.get('button#userFilter')
+    .click();
+
+    cy.get('input#inputUtente').first().type("Mario");
+
+    cy.get('a.listutenti')
+    .first()
+    .click(); 
+
+    cy.get('button#btnFiltriA')
+    .click();
+
+    cy.wait(1000);
+
+    cy.get('div.card2')
+    .then(cards => { 
+      expect(cards.length).equal(cardsExpected);
+    });
+  }); 
+  });
+
+  it('testing object Tavolo filter', () => {
+    
+    cy.get('#carouselExampleIndicators div.card').then(cards => {
+      const initialElements = cards.length;
+      const cardsExpected = 1;
+    cy.get('button#btnFiltriF')
+    .click(); 
+
+    cy.get('button#nomeOggettoFilter')
+    .click();
+
+    cy.get('input#inputOggetto').first().type("Tavolo");
+
+    cy.get('a.listoggetti')
+    .first()
+    .click(); 
+
+    cy.get('button#btnFiltriA')
+    .click();
+
+    cy.wait(1000);
+
+    cy.get('div.card2')
+    .then(cards => { 
+      expect(cards.length).equal(cardsExpected);
+    });
+  }); 
+  });
 
 });
