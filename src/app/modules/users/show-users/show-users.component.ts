797dfa58d46d080e46fab5105f1005954111f729
Leonardo Capogna <leonardocapogna@gmail.com>
2021-11-29 16:38:05 +0100

    filtro e evidenziatore dalla search

--- src/app/modules/users/show-users/show-users.component.ts
+++ src/app/modules/users/show-users/show-users.component.ts
@@ -11,11 +11,6 @@ export class ShowUsersComponent implements OnInit {
   utenti: Array<User>=[]
 
   constructor(private route:ActivatedRoute, private api: UsersService) {
-    // this.api.getUsers().subscribe(users=>{
-    //   this.utenti = users;
-    //   console.log("ho caricato", this.utenti )
-
-    // })
     this.route.data.subscribe(data => {
       this.utenti = data.utente;
     }, error => {
@@ -29,32 +24,46 @@ export class ShowUsersComponent implements OnInit {
 
   cercati : Array<User> = [];
   
+
   ricerca() : void{
-    // let cerca = document.getElementById("cercaNome") as HTMLInputElement;
-    // this.api.getUsers(1, undefined, undefined, cerca.value + " ").subscribe(value => {
-    //   this.cercati = value;
-    // },);
-    // console.log(this.cercati);
-console.log("gli utenti sono:", this.utenti);
+    //console.log("gli utenti sono:", this.utenti);
+    this.cercati = [];
 
     let cerca = document.getElementById("cercaNome") as HTMLInputElement;
     for (let i = 0; i < this.utenti.length; i++) {
-     if (this.utenti[i].name == cerca.value) {
+     if (this.utenti[i].name == cerca.value || this.utenti[i].surname == cerca.value || this.utenti[i].email == cerca.value|| this.utenti[i].name + " " + this.utenti[i].surname == cerca.value) {
         this.cercati.push(this.utenti[i]);
-      }
+      }  
     }
-    console.log("gli utenti trovati sono:" , this.cercati);
+    if (this.cercati[0] == null) {
+      alert("utente non trovato");
+    }
+    //console.log("gli utenti trovati sono:" , this.cercati);
   }
-
-  stampaNomeTrovato(utente : User) : boolean{
+    stampaNomeTrovato(utente : User) : boolean{
+      
     let controllo = false;
 
     for (let i = 0; i < this.cercati.length; i++) {
       if (this.cercati[i].name == utente.name) {
         controllo = true;
         break;
-       }
-     }
+      }
+    }
      return controllo;
+  }
+
+  mostra = true;
+
+  aggiornaMostra(){
+     this.mostra = !this.mostra;
+  }
+
+   devoMostrare(){
+     return this.mostra;
+   }
+
+   evidenzio(controllo : boolean){
+    return {'bg-danger !important' : controllo}
   }
 }
