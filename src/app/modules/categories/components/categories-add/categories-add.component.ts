953c8816e888c619c8d9c828ca67c3cfc0736fc5
sumrge <sumrge41@protonmail.com>
2021-11-29 14:45:34 +0100

    add clean

--- src/app/modules/categories/components/categories-add/categories-add.component.ts
+++ src/app/modules/categories/components/categories-add/categories-add.component.ts
@@ -26,7 +26,7 @@ export class CategoriesAddComponent implements OnInit {
     var category: Category = {
       id: null, //per fare in modo che venga automaticamente inserito dall'app
       name: name, //"name" di Category è pari al valore della variabile "name" sopra
-      description: description, //idem name
+      description: description
     };
 
     var nomecat = name.toLowerCase();
@@ -49,82 +49,13 @@ export class CategoriesAddComponent implements OnInit {
         this.categoryService
          .createCategory(category)
          .subscribe(category => console.log('Nuova categoria:', category));
-      }// fine if..else
-    });//fine subscribe()
+      }// fine if...else
+    });// fine subscribe()
+  }// fine onSubmit()
 
-//       for (const item of categories) {
-//         this.name = item.name;
-// /*la some() nella categories*/
-//         function doesItMatch() {
-//           return nomecat === item.name.toLowerCase();
-//         }
-//         var match = [item.name.toLowerCase()].some(doesItMatch);
-//
-//         if (match == true) {
-//           const element: HTMLElement = document.getElementById('nameMatch') as HTMLElement;
-//           element.style.display = 'block';
-//           var button = <HTMLButtonElement>document.getElementById('button');
-//           button.disabled = true;
-//
-//           this.controlloGlobal = false;
-//           console.log(this.controlloGlobal);
-//         } // fine if
-//       } // fine for
-//       if (this.controlloGlobal === true) {
-//         this.categoryService
-//           .createCategory(category)
-//           .subscribe(category => console.log('Nuova categoria:', category));
-//       } // fine if
-//     }); // fine subscribe()
-
-    // if (this.controlloGlobal === true) {
-    //   this.categoryService
-    //     .createCategory(category)
-    //     .subscribe(category => console.log('Nuova categoria:', category));
-    // } // fine if
-  } // fine onSubmit()
-
-
-
-
-
+  // per tornare alla pagina precedente al click del bottone
   goBack(): void {
     this.router.navigate(['categories']);
   }
 
-  //controllo che non esista già una categoria con lo stesso nome
-  name = '';
-
-  // controlloGlobal: boolean = false;
-  //
-  // testNome(formadd: NgForm): boolean {
-  //   var controllo: boolean = true;
-  //
-  //   var nomecat = formadd.controls['name'].value.toLowerCase();
-  //   //console.log(nomecat); FUNZIONA
-  //   //
-  //   this.categoryService.getCategories().subscribe(categories => {
-  //     for (const item of categories) {
-  //       this.name = item.name;
-  //
-  //       function doesItMatch() {
-  //         return nomecat === item.name.toLowerCase();
-  //       }
-  //       var match = [item.name.toLowerCase()].some(doesItMatch);
-  //
-  //       if (match == true) {
-  //         const element: HTMLElement = document.getElementById('nameMatch') as HTMLElement;
-  //         element.style.display = 'block';
-  //         var button = <HTMLButtonElement>document.getElementById('button');
-  //         button.disabled = true;
-  //
-  //         controllo = false;
-  //         this.controlloGlobal = controllo;
-  //       } // fine if
-  //     } // fine for
-  //   }); // fine subscribe()
-  //
-  //   console.log("la variabile è pari a " + this.controlloGlobal);
-  //   return this.controlloGlobal;
-  // } // fine testNome()
 }
