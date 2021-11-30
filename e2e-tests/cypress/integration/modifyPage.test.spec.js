28b79d6fb2c2692f697625acc00659628cdd559b
serena <serena.russo8@studio.unibo.it>
2021-11-30 11:00:49 +0100

    e2e modify

--- e2e-tests/cypress/integration/modifyPage.test.spec.js
+++ e2e-tests/cypress/integration/modifyPage.test.spec.js
@@ -8,41 +8,67 @@ describe('modifyPage tests', () => {
   
 it('closing carousel and open Modify page after clicking Modifica Oggetto button', () => {
      
-  cy.get('a.modifyItem a') 
+  cy.get('.carousel-item')
+  .first()
+  .find('button')
+  .first()
+  .click()
+  .get('.dropdown-item')
+  .eq(1)
   .click();
 
-  cy.url().should('contain', 'modifica-oggetto'); 
+ cy.url().should('contain', 'modifica-oggetto'); 
 });
  
 
 it('clicking on the button Invia to check if the button is visible', () => {
-  cy.get('a.modifyItem a') 
+  cy.get('.carousel-item')
+  .first()
+  .find('button')
+  .first()
+  .click()
+  .get('.dropdown-item')
+  .eq(1)
   .click();
 
-  cy.get('button#inviabottone').should('not.be.enabled'); 
+  cy.get('button#inviabottone').should('be.enabled'); 
 });
 
 it('inserting data, clicking on the button Invia to visualize alert message and go back to the item page list', () => {
-  cy.get('a.modifyItem a') 
+  it('inserting data, clicking on the button Invia to visualize alert message and go back to the item page list', () => {
+    cy.get('.carousel-item')
+  .first()
+  .find('button')
+  .first()
+  .click()
+  .get('.dropdown-item')
+  .eq(1)
   .click();
 
-  cy.get('input#name_input').type('Nome');
-  cy.get('input#description_input').type('Descrizione');
-  cy.get('input#price_input').type('2'); 
+    cy.get('input#nameinput').type('Nome');
+    cy.get('input#descriptioninput').type('Descrizione');
+    cy.get('input#price_input').type('2'); 
 
-  cy.get('button#inviabottone')
-  .click()
-  
+    cy.get('button#inviabottone')
+    .click()
+    
 
-  cy.get('div#alertsuccessdiv').should('be.visible');
-  cy.wait(2000);
-  cy.url().should('not.contain', 'aggiungi-oggetto')
-  .and('contain','items');
+    cy.get('div#alertsuccessdiv').should('be.visible');
+    cy.wait(2000);
+    cy.url().should('not.contain', 'aggiungi-oggetto')
+    .and('contain','items');
+  });
 });
 
 it('clicking on the button Carica Immagine to visualize image', () => {
-  cy.get('a.modifyItem a') 
-  .click();
+  cy.get('.carousel-item')
+  .first()
+  .find('button')
+  .first()
+  .click()
+  .get('.dropdown-item')
+  .eq(1)
+  .click();;
 
   cy.get('input#url').type('https://montagnolirino.it/wp-content/uploads/2015/12/immagine-non-disponibile-300x225.png');
    
@@ -56,7 +82,13 @@ it('clicking on the button Carica Immagine to visualize image', () => {
 });
 
 it('clicking on the button Annulla to go back to the item page list', () => {
-  cy.get('div.addItem a') 
+  cy.get('.carousel-item')
+  .first()
+  .find('button')
+  .first()
+  .click()
+  .get('.dropdown-item')
+  .eq(1)
   .click();
 
   cy.get('button#indietrobottone')
